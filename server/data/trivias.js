4080fb41b6137dc57d68044773b3a96b788de702
LUIS AREVALO <luisgabrielarevalo@hotmail.com>
2022-01-04 18:05:03 -0500

    (backend) add trivia data

---
+++ server/data/trivias.js
@@ -0,0 +1,281 @@
+const trivias = [
+    {
+        question: "¿Cuál es el río más largo del mundo?",
+        answers:[
+            "Río Nilo",
+            "Río Rin",
+            "Río Magadalena",
+            "Río Amazonas"
+        ],
+        correctAnswer:0,
+        level: 1
+    },
+    {
+        question: "¿Cuál es el animal nacional de Australia?",
+        answers:[
+            "El perro",
+            "El elefante",
+            "El canguro",
+            "El pato"
+        ],
+        correctAnswer:2,
+        level:1
+    },
+    {
+        question: '¿Quién escribió "Cien años de soledad"?',
+        answers:[
+            "Pablo Neruda",
+            "Gabriel García Marquez",
+            "Edgar Allan Poe",
+            "Franz Kafka"
+        ],
+        correctAnswer:1,
+        level:1
+    },
+    {
+        question: '¿De qué país es el jugador de fútbol Luis Suárez?',
+        answers:[
+            "Perú",
+            "Colombia",
+            "Canadá",
+            "Uruguay"
+        ],
+        correctAnswer:3,
+        level:1
+    },
+    {
+        question: '¿A qué velocidad viaja la luz?',
+        answers:[
+            "100 km/s",
+            "150 mil km/s",
+            "300 km/s",
+            "La luz no viaja"
+        ],
+        correctAnswer:2,
+        level:1
+    },
+
+    {
+        question: '¿De qué país forma parte Hawaii?',
+        answers:[
+            "Estados Unidos",
+            "Canadá",
+            "China",
+            "Rusia"
+        ],
+        correctAnswer:0,
+        level:2
+    },
+    {
+        question: '¿Un lustro cuántos años son?',
+        answers:[
+            "1 año",
+            "10 años",
+            "3 años",
+            "5 años"
+        ],
+        correctAnswer:3,
+        level:2
+    },
+    {
+        question: '¿En qué deporte destaca Tiger Woods?',
+        answers:[
+            "Voleyball",
+            "Basketball",
+            "Golf",
+            "Tenis"
+        ],
+        correctAnswer:2,
+        level:2
+    },
+    {
+        question: '¿Cuántos puntos vale un tiro libre en el basket?',
+        answers:[
+            "1 punto",
+            "2 puntos",
+            "3 puntos",
+            "5 puntos"
+        ],
+        correctAnswer:0,
+        level:2
+    },
+    {
+        question: '¿Qué país ha ganado más mundiales de fútbol? ',
+        answers:[
+            "Italia",
+            "Alemania",
+            "Brasil",
+            "Colombia"
+        ],
+        correctAnswer:2,
+        level:2
+    },
+
+    {
+        question: '¿Cuántos gramos son 100 mililitros?',
+        answers:[
+            "100 gramos",
+            "10 gramos",
+            "1000 gramos",
+            "1 gramo"
+        ],
+        correctAnswer:0,
+        level:3
+    },
+    {
+        question: '¿Quién descubrió la penicilina?',
+        answers:[
+            "Marie Curie",
+            "Louis Pasteur",
+            "Alexaner Fleming",
+            "Nicola Tesla"
+        ],
+        correctAnswer:2,
+        level:3
+    },
+    {
+        question: '¿Cuál es el océano más grande del mundo?',
+        answers:[
+            "Oceano Atlántico",
+            "Oceano Índico",
+            "Oceano Glacial Ártico",
+            "Nicola Pacífico"
+        ],
+        correctAnswer:3,
+        level:3
+    },
+    {
+        question: '¿Cuándo acabó la II Guerra Mundial?',
+        answers:[
+            "1945",
+            "1949",
+            "1939",
+            "1925"
+        ],
+        correctAnswer:0,
+        level:3
+    },
+    {
+        question: '¿Cuántos años duró la guerra de los 100 años? ',
+        answers:[
+            "100 años",
+            "90 años",
+            "95 años",
+            "116 años"
+        ],
+        correctAnswer:3,
+        level:3
+    },
+
+    {
+        question: '¿Quién fue el padre de la bomba atómica?',
+        answers:[
+            "Robert Oppenheimer",
+            "Albert Einstein",
+            "Marie Curie",
+            "Thomas Alba Edisson"
+        ],
+        correctAnswer:0,
+        level:4
+    },
+    {
+        question: '¿Cuál es el origen de la música rock?',
+        answers:[
+            "El Rap",
+            "El Blues",
+            "El Jazz",
+            "El Heavy Metal"
+        ],
+        correctAnswer:1,
+        level:4
+    },
+    {
+        question: '¿Cuánto mide el David de Miguel Ángel?',
+        answers:[
+            "1.8 metros",
+            "2.7 metros",
+            "4.2 metros",
+            "5.2 metros"
+        ],
+        correctAnswer:3,
+        level:4
+    },
+    {
+        question: '¿Cuándo se extinguieron los dinosaurios?',
+        answers:[
+            "Al final del periodo Jurásico",
+            "Al inicio del periodo Peleolítico",
+            "Al final del periodo Cretácico",
+            "A mediados del periodo Triásico"
+        ],
+        correctAnswer:2,
+        level:4
+    },
+    {
+        question: '¿Cuál es el ser vivo más grande del mundo?',
+        answers:[
+            "El hongo Armillaria",
+            "La ballena azul",
+            "La jirafa",
+            "El tiburón ballena"
+        ],
+        correctAnswer:0,
+        level:4
+    },
+
+    {
+        question: '¿Quién fue la primera alpinista en llegar al Everest?',
+        answers:[
+            "Katherin Swartz",
+            "Junko Tabei",
+            "Andrea Larsson",
+            "Keiko Mirogi"
+        ],
+        correctAnswer:1,
+        level:5
+    },
+    {
+        question: '¿Cuándo es el equinoccio de primavera?',
+        answers:[
+            "Entre el 6 y el 8 de agosto",
+            "Entre el 20 y el 25 de junio",
+            "Entre el 1 y el 25 de noviembre",
+            "Entre el 19 y 21 de marzo"
+        ],
+        correctAnswer:3,
+        level:5
+    },
+    {
+        question: '¿Dónde están ubicados los huesos escafoides? ',
+        answers:[
+            "En la muñeca",
+            "En el oido interno",
+            "En la columna vertebral",
+            "En el pie"
+        ],
+        correctAnswer:0,
+        level:5
+    },
+    {
+        question: '¿Dónde se realizó el primer trasplante de corazón humano?',
+        answers:[
+            "En Alemania",
+            "En Estados Unidos",
+            "En Sudáfrica",
+            "En Francia"
+        ],
+        correctAnswer:2,
+        level:5
+    },
+    {
+        question: '¿Cuál es el nombre de la época geológica en la que nos encontramos actualmente?',
+        answers:[
+            "Palioceno",
+            "Pleistoceno",
+            "Holoceno",
+            "Angioseno"
+        ],
+        correctAnswer:2,
+        level:5
+    },
+]
