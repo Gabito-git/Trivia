62287396f71f4da9a18659f987227b0189f2d548
LUIS AREVALO <luisgabrielarevalo@hotmail.com>
2022-01-04 13:28:29 -0500

    feat(front) add gamescreen styling

--- client/src/screens/GameScreen.js
+++ client/src/screens/GameScreen.js
@@ -1,18 +1,35 @@
+import presenter from '../assets/presenter.png'
+
 const GameScreen = () => {
     return (
         <div className="gamescreen">
-            <div className="gamescreen__presenter-talk"></div>
-            <div className="gamescreen__question"></div>
-            <div className="gamescreen__answers-1">
-                <div className="gamescreen__answer"></div>
-                <div className="gamescreen__answer"></div>
+            <div className="gamescreen__presenter-talk">
+                <p>Vamos por $ 100.000</p>
+                <div className="gamescreen__presenter">
+                    <img src={ presenter } alt=""/>
+                </div>
+            </div>
+            <div className="gamescreen__question">
+                <p>Cual es la diferencia entre doña Ramona y una marrana mona?</p>
+            </div>
+            <div className="gamescreen__answers">
+                <div className="gamescreen__answer">
+                    <p>A. No sabo nadita</p>
+                </div>
+                <div className="gamescreen__answer">
+                    <p>B. Ni idea</p>
+                </div>
             </div>
 
-            <div className="gamescreen__answers-2">
-                <div className="gamescreen__answer"></div>
-                <div className="gamescreen__answer"></div>
+            <div className="gamescreen__answers">
+                <div className="gamescreen__answer">
+                    <p>C. De que me hablas viejo?</p>
+                </div>
+                <div className="gamescreen__answer">
+                    <p>D. Uhmmmm yalas yalas!!!!</p>
+                </div>
             </div>
-            <div className="gamescreen__presenter"></div>
+            
         </div>
     )
 }
