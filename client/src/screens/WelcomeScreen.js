a9aff8e1fff551a85c5d1e5148f6b6f66e9ec946
LUIS AREVALO <luisgabrielarevalo@hotmail.com>
2022-01-04 13:36:19 -0500

    (front) add basic welcomescreen component

---
+++ client/src/screens/WelcomeScreen.js
@@ -0,0 +1,23 @@
+const WelcomeScreen = () => {
+    return (
+        <div className="welcomescreen">
+            <div className="welcomescreen__banner">
+                <h1 className="welcomescreen__title">Trivia</h1>
+                <span className="welcomescreen__sutitle">Millonaria</span>
+            </div>
+            <form>
+                <div className="welcomescreen__input-div">
+                    <input 
+                        placeholder="ingresa tu apodo"
+                        className="welcomescreen__input"
+                    />
+                </div>
+                <div className="welcomescreen__button-div">
+                    <button className="welcomescreen__button">Iniciar</button>
+                </div>
+            </form>            
+        </div>
+    )
+}
+
+export default WelcomeScreen
