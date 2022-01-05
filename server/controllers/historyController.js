98c8ce29f964618af95a26d9a68747aca4735b9b
LUIS AREVALO <luisgabrielarevalo@hotmail.com>
2022-01-05 12:00:59 -0500

    (backend) add try/catch block to controllers

--- server/controllers/historyController.js
+++ server/controllers/historyController.js
@@ -3,19 +3,44 @@ const User = require("../models/users");
 const addUserToHistory = async(req, res) => {
     const { username, score } = req.body;
 
-    const user = new User({
-        username, score
-    })
-
-    const newUser = await user.save();
-
-    res.status(201).json( newUser );
+    try {
+        const user = new User({
+            username, score
+        })
+    
+        const newUser = await user.save();
+    
+        res.status(201).json( {
+            ok: true,
+            user: newUser
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            ok: false,
+            message: 'Please contact your administrator'
+        })
+    }
+    
 }
 
 const getHistory = async(req, res) => {
-    const history = await User.find();
 
-    res.status(200).json(history)
+    try {
+        const history = await User.find();
+        res.status(200).json({
+            ok: true,
+            history
+        })
+        
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            ok: false,
+            message: 'Please contact your administrator'
+        })
+    }
+    
 }
 
 module.exports ={
--- server/controllers/triviaController.js
+++ server/controllers/triviaController.js
@@ -3,10 +3,22 @@ const Trivia = require('../models/trivias');
 const getTrivia = async( req, res ) => {
     const { level } = req.params;
 
-    const trivias = await Trivia.find({ level });
-    const triviaToReturn = trivias[Math.floor(Math.random()*5)]
+    try {
+        const trivias = await Trivia.find({ level });
+        const triviaToReturn = trivias[Math.floor(Math.random()*5)]
 
-    res.status(200).json( triviaToReturn );    
+        res.status(200).json( {
+            ok: true,
+            trivia: triviaToReturn
+        } );   
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            ok:false,
+            message: 'Please contact your administrator'
+        })
+    }
+     
 }
 
 module.exports = {
