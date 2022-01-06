ec169c4bb056d0e433f3c35824e4a731197de96d
LUIS AREVALO <luisgabrielarevalo@hotmail.com>
2022-01-06 08:14:54 -0500

    feat(front) add history box and history request code

--- client/src/screens/WelcomeScreen.js
+++ client/src/screens/WelcomeScreen.js
@@ -1,21 +1,52 @@
+import { useEffect, useState } from "react";
+
 const WelcomeScreen = () => {
+
+    const [history, setHistory] = useState([]);
+
+    useEffect(() => {
+        const getHistory = async() => {
+            const response = await fetch(
+                'http://localhost:4000/api/history',{
+                    method: 'get',
+                    'Content-Type':'Application/json'
+                }
+            )
+
+            const data = await response.json();
+        }
+
+        getHistory()
+    }, [])
+
     return (
         <div className="welcomescreen">
-            <div className="welcomescreen__banner">
-                <h1 className="welcomescreen__title">Trivia</h1>
-                <span className="welcomescreen__subtitle">Millonaria</span>
+            <div className="welcomescreen__history">
+                <h2>Mejores concursantes</h2>
+                <ul>
+                    <li>Gabito $100.000.000</li>
+                    <li>Dianlo $5.000.000</li>
+                </ul>
             </div>
-            <form>
-                <div className="welcomescreen__input-div">
-                    <input 
-                        placeholder="ingresa tu apodo"
-                        className="welcomescreen__input"
-                    />
+            <div className="welcomeScreen__start">
+                <div className="welcomescreen__banner">
+                    <h1 className="welcomescreen__title">Trivia</h1>
+                    <span className="welcomescreen__subtitle">Millonaria</span>
                 </div>
-                <div className="welcomescreen__button-div">
-                    <button className="welcomescreen__button">Iniciar</button>
-                </div>
-            </form>            
+                <form>
+                    <div className="welcomescreen__input-div">
+                        <input 
+                            placeholder="ingresa tu apodo"
+                            className="welcomescreen__input"
+                        />
+                    </div>
+                    <div className="welcomescreen__button-div">
+                        <button className="welcomescreen__button">Iniciar</button>
+                    </div>
+                </form> 
+
+            </div>
+                       
         </div>
     )
 }
